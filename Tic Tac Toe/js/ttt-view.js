15131c9a5e012da8c56189a02f773f096b058702
App Academy Student <student@appacademy.io>
2017-09-05 15:42:40 -0700

    make cells toggle on click

--- Tic Tac Toe/js/ttt-view.js
+++ Tic Tac Toe/js/ttt-view.js
@@ -4,18 +4,31 @@ class View {
     this.$el = $el;
 
     this.setupBoard($el);
+    this.bindEvents(game);
   }
 
-  bindEvents() {}
+  bindEvents(game) {
+    $(".cell").on("click", (event) => {
+      let pos = $(event.currentTarget).data("pos");
+      game.playMove(pos);
+      this.makeMove($(event.currentTarget));
+      // event.currentTarget.css("background-color: green");
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    $square.removeClass("not_selected");
+    $square.toggleClass("selected_x");
+  }
 
   setupBoard($el) {
     const board = $el.append($('<ul></ul>'));
 
     for (var i = 0; i < 3; i++) {
       for (var j = 0; j < 3; j++) {
-        board.append('<li></li>');
+        let $myLi = $('<li class="cell not_selected"></li>');
+        $myLi.data("pos", [i,j]);
+        board.append($myLi);
       }
     }
   }
